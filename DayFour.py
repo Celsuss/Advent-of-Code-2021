df40d0421641aa9fb0d2e9473a4b6d31b83a27bb
Jens Lordén <jenslorden@gmail.com>
2022-05-21 17:04:07 +0200

    Added solution for day four part one

---
+++ DayFour.py
@@ -0,0 +1,137 @@
+import numpy as np
+
+def getData(path):
+    def getNextMoves(lines, index):
+        moves = []
+        line = lines[index]
+        while line != '\n':
+            line = line.split('\n')[0]
+            moves.extend(line.split(','))
+            index += 1
+            line = lines[index]
+        return moves, index
+
+    def getNextBoard(lines, index):
+        board = []
+        line = lines[index]
+        while line != '\n':
+            line = line.split('\n')[0].split(' ')
+            while '' in line:
+                line.remove('')
+            board.append(line)
+            index += 1
+            if index >= len(lines):
+                break
+            line = lines[index]
+        return board, index
+
+    moves = []
+    boards = []
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        i = 0
+
+        moves, i = getNextMoves(lines, i)
+        i += 1
+        
+        while i < len(lines):
+            board, i = getNextBoard(lines, i)
+            boards.append(board)
+            i += 1
+    
+    return moves, boards
+
+def processMove(move, boards_dict, boards_score):
+    for board_dict, board_score in zip(boards_dict, boards_score):
+        if move in board_dict:
+            position = board_dict[move]
+            board_score[position[0]-1, position[1]-1] += 1
+
+    return
+
+def getHorizontalScore():
+    return
+
+def getVerticalScore():
+    return
+
+def detectBingo(boards_dict, boards_score):
+    for board_dict, board_score in zip(boards_dict, boards_score):
+        for row in board_score:
+            if np.sum(row) == board_score.shape[0]:
+                # Bingo in a row detected
+                print('Bingo!')
+                return board_dict, board_score
+
+        for i in range(board_score.shape[1]):
+            if np.sum(board_score[:,i]) == board_score.shape[1]:
+                print('Bingo!')
+                return board_dict, board_score
+
+        continue
+
+    return None, None
+
+def getSumOfUnmarkedNumbers(winning_board_dict, winning_board_score):
+    sum = 0
+    for key in winning_board_dict:
+        position = winning_board_dict[key]
+        if winning_board_score[position[0]-1, position[1]-1] == 0:
+            sum += int(key)
+
+    return sum
+
+def generateBoardScore(boards):
+    board_score = np.zeros((len(boards), len(boards[0]), len(boards[0][0])))
+    return board_score
+
+def createBoardDict(board):
+    board_dict = {}
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            board_dict[board[i][j]] = (i, j)
+
+    return board_dict
+
+def createBoardsDicts(boards):
+    boards_dicts = []
+    for board in boards:
+        boards_dicts.append(createBoardDict(board))
+
+    return boards_dicts
+
+def playBingo(moves, boards):
+    boards_score = generateBoardScore(boards)
+    boards_dict = createBoardsDicts(boards)
+    
+    for move in moves:
+        processMove(move, boards_dict, boards_score)
+        winning_board_dict, winning_board_score = detectBingo(boards_dict, boards_score)
+        if winning_board_dict is not None:
+            return winning_board_dict, winning_board_score, int(move)
+
+        continue
+
+    return None, None, None
+
+def main():
+    # Part one test
+    moves, boards = getData('data/4-test.txt')
+    winning_board_dict, winning_board_score, winning_move = playBingo(moves, boards)
+    sum = getSumOfUnmarkedNumbers(winning_board_dict, winning_board_score)
+    assert sum == 188
+    assert winning_move == 24
+    final_product = sum * winning_move
+    assert final_product == 4512
+
+    # Part one
+    moves, boards = getData('data/4.txt')
+    winning_board_dict, winning_board_score, winning_move = playBingo(moves, boards)
+    sum = getSumOfUnmarkedNumbers(winning_board_dict, winning_board_score)
+    final_product = sum * winning_move
+    print('Part one:', final_product)
+
+    return
+
+if __name__ == '__main__':
+    main()
